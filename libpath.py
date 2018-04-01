cd240427a0b3f1ab01331fc72b194debfffa58c4
Danilo Horta <danilo.horta@gmail.com>
2018-04-01 14:26:57 +0100

    Example syntax

--- libpath.py
+++ libpath.py
@@ -18,14 +18,14 @@ Example
     >>> from libpath import Windows, Unix
     >>>
     >>> if platform.system() == 'Windows':
-    >>>     s = Windows()
-    >>>     f = s.get_programfiles()
-    >>>     s.add_library_dir(join(f, 'zstd', 'lib'))
-    >>>     s.add_include_dir(join(f, 'zstd', 'include'))
-    >>>     libs = [s.find_libname('zstd')]
-    >>> else:
-    >>>     s = Unix()
-    >>>     libs = ['zstd']
+    ...     s = Windows()
+    ...     f = s.get_programfiles()
+    ...     s.add_library_dir(join(f, 'zstd', 'lib'))
+    ...     s.add_include_dir(join(f, 'zstd', 'include'))
+    ...     libs = [s.find_libname('zstd')]
+    ... else:
+    ...     s = Unix()
+    ...     libs = ['zstd']
     >>>
     >>> include_dirs = s.get_include_dirs()
     >>> library_dirs = s.get_library_dirs()
